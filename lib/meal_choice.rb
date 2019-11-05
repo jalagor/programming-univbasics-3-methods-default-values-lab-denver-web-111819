a29b0f48e7c1228f3c05f78831e3928ecad5cbfa
John Rogala <johnrossrogala@gmail.com>
2019-11-05 18:10:48 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,5 @@ def meal_choice(veg1, veg2, protein = 'meat')
   puts "A plate of #{protein}, with #{veg1} and #{veg2}."
 end
 
-order = meal_choice("broccoli", "macaroni") = true
-order
+meal_choice("broccoli", "macaroni") = true
  
