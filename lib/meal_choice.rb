4135c34c57242b4560897954c1df6436da559ad1
John Rogala <johnrossrogala@gmail.com>
2019-11-05 18:51:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 def meal_choice(veg1, veg2, protein = 'meat')
   p "What a nutritious meal!"
   p "A plate of #{protein} with #{veg1} and #{veg2}."
+  order = meal_choice
 end
 
 meal_choice("broccoli", "macaroni", "beef") 
